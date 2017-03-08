b71c6f94b9a4e67b0b52838da37b04410660525f
Libert R Schmidt <rschmidt@nuvops.com>
2017-03-07 20:08:46 -0500

    vpc/network

---
+++ vpc/lookup_plugins/get_azs.py
@@ -0,0 +1,11 @@
+from ansible.plugins.lookup import LookupBase
+import boto3
+
+class LookupModule(LookupBase):
+
+    def run(self,terms, inject=None, **kwargs):
+
+        client = boto3.client('ec2',region_name=terms[0])
+        azs = [ az['ZoneName'] for az in client.describe_availability_zones()['AvalabilityZones']]
+
+        return azs
